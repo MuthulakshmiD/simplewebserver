8718be18f873d25d1d827c21dd091fe5414e34e2
MuthulakshmiD <mmuthulakshmi063@gmail.com>
2023-11-08 21:58:38 -0800

    Success

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # EX01 Developing a Simple Webserver
-## Date:7/11/23
+## Date: 07.10.23
+
 
 ## AIM:
 To develop a simple webserver to serve html pages.
@@ -21,53 +22,69 @@ Serving the HTML pages.
 Testing the webserver.
 
 ## PROGRAM:
-```
-<html>
-  <title> image map</title>
-     <body>
-        <table border="4" cellspacing="8" cellspacing="7">
-          <caption="centre"> TOP FIVE GENERATING SOFTWARE COMPANIES </caption>
-            <tr>
-               <th>S.No</th>
-               <th>Company</th>
-               <th>Revenue</th>
-             </tr>
-             <tr>
-               <th>1</th>
-               <td>Microsoft</td>
-               <td>65 Billion</td>
-             </tr>
-             <tr>
-               <th>2</th>
-               <td>Oracle</td>
-               <td>29.6 Billion</td>
-             </tr>
-             <tr>
-               <th>3</th>
-               <td>IBM</td>
-               <td>29.1 Billion</td>
-             </tr>
-             <tr>
-               <th>4</th>
-               <td>SAP</td>
-               <td>6.4 Billion</td>
-             </tr>
-             <tr>
-               <th>S.No</th>
-               <td>Symantec</td>
-               <td>5.6 Billion</td>
-             </tr>
-           </table border>
-      </body>
-</html>
-```
 
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>My webserver</title>
+</head>
+<body>
+<table border="2">
+<caption> Top Five Revenue Generating Software Companies</caption>
+<tr>
+<th>S.No</th>
+<th>Company</th>
+<th>Revenue</th>
+</tr>
+<tr>
+<td>1</td>
+<td>Microsoft</td>
+<td>65 Billion</td>
+</tr>
+<tr>
+<td>2</td>
+<td>Oracle</td>
+<td>29.6 Billion</td>
+</tr>
+<tr>
+<td>3</td>
+<td>IBM</td>
+<td>29.1 Billion</td>
+</tr>
+<tr>
+<td>4</td>
+<td>SAP</td>
+<td>6.4 Billion</td>
+</tr>
+<tr>
+<td>5</td>
+<td>Symantec</td>
+<td>5.6 Billion</td>
+</tr>
+</table>
+</body>
+</html>
+"""
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address = ('',8000)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
+httpd.serve_forever()
 
 
 ## OUTPUT:
+
 ![Alt text](Simplewebserver.png)
-![Alt text](<web a.png>)
+![Alt text](<Screenshot 2023-11-08 214315.png>)
 
 
 ## RESULT:
-The program for implementing simple webserver is executed successfully
+The program for implementing simple webserver is executed successfully.
--- web.py
+++ web.py
@@ -2,93 +2,45 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <!DOCTYPE html>
 <html>
-    <title> Software Companies </title>
-     <body>
-          <table border="4" cellspacing="20" cellspacing="15">
-            <caption="centre"> Top Five Generating Software Companies </caption>
-             <tr>
-                 <th>S.No</th>
-                 <th>Company</th>
-                 <th>Revenue</th>
-             </tr>
-             <tr>
-                 <th>1</th>
-                 <td>Microsoft</td>          
-                 <td>65 Billion</td>
-              </tr>
-              <tr>
-                 <th>2</th>
-                 <td>Oracle</td>             
-                 <td>29.6 Billlion</td>
-              </tr>
-              <tr>
-                 <th>3</th>
-                 <td>IBM</td>          		
-                 <td>29.1 Billon</td>
-              </tr>
-              <tr>
-                 <th>4</th>
-                 <td>SAP</td>
-                 <td>6.4 Billion</td> 
-              </tr>
-                  <th>5</th>
-                  <td>Symantec</td>
-                  <td>5.6 Billon</td>
-              </tr>
-             </table border>
-        </body>    
-</html>  
-"""
-class myhandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        print("request received")
-        self.send_response(200)
-        self.send_header('content-type', 'text/html; charset=utf-8')
-        self.end_headers()
-        self.wfile.write(content.encode())
-server_address = ('',8000)
-httpd = HTTPServer(server_address,myhandler)
-print("my webserver is running...")
-httpd.serve_forever()from http.server import HTTPServer, BaseHTTPRequestHandler
-content = """
-<!DOCTYPE html>
-<html>
-    <title> Software Companies </title>
-     <body>
-          <table border="4" cellspacing="20" cellspacing="15">
-            <caption="centre"> Top Five Generating Software Companies </caption>
-             <tr>
-                 <th>S.No</th>
-                 <th>Company</th>
-                 <th>Revenue</th>
-             </tr>
-             <tr>
-                 <th>1</th>
-                 <td>Microsoft</td>          
-                 <td>65 Billion</td>
-              </tr>
-              <tr>
-                 <th>2</th>
-                 <td>Oracle</td>             
-                 <td>29.6 Billlion</td>
-              </tr>
-              <tr>
-                 <th>3</th>
-                 <td>IBM</td>          		
-                 <td>29.1 Billon</td>
-              </tr>
-              <tr>
-                 <th>4</th>
-                 <td>SAP</td>
-                 <td>6.4 Billion</td> 
-              </tr>
-                  <th>5</th>
-                  <td>Symantec</td>
-                  <td>5.6 Billon</td>
-              </tr>
-             </table border>
-        </body>    
-</html>  
+<head>
+<title>My webserver</title>
+</head>
+<body>
+<table border="2">
+<caption> Top Five Revenue Generating Software Companies</caption>
+<tr>
+<th>S.No</th>
+<th>Company</th>
+<th>Revenue</th>
+</tr>
+<tr>
+<td>1</td>
+<td>Microsoft</td>
+<td>65 Billion</td>
+</tr>
+<tr>
+<td>2</td>
+<td>Oracle</td>
+<td>29.6 Billion</td>
+</tr>
+<tr>
+<td>3</td>
+<td>IBM</td>
+<td>29.1 Billion</td>
+</tr>
+<tr>
+<td>4</td>
+<td>SAP</td>
+<td>6.4 Billion</td>
+</tr>
+<tr>
+<td>5</td>
+<td>Symantec</td>
+<td>5.6 Billion</td>
+</tr>
+</table>
+</body>
+</html>
 """
 class myhandler(BaseHTTPRequestHandler):
     def do_GET(self):
